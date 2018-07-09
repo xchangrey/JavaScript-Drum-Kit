163a4a5e9d2ba1fc7917b32920ce0fd27c1ab9ff
Christian Geroy <cr50.christian@enfrausaph.com>
2018-07-09 15:26:31 -0400

    Modified the playSound fn

--- drumkit.js
+++ drumkit.js
@@ -1,11 +1,10 @@
 function playSound(e){
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	const key = document.querySelector(`div[data-key="${e.which}"]`);
-
+	const letter = document.querySelector(`div[data-key="${e.keyCode}"]`);
 	if(!audio) return;
 	audio.currentTime = 0;
 	audio.play();
-	key.classList.add('playing');
+	letter.classList.add('playing');
 }
 
 function removeTransition(e){
